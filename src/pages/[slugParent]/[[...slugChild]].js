533e71ab9aceb3df3620bb0ae218d2488b4b6ac7
bdanielmr <bdanielmr@gmail.com>
2021-11-07 22:48:11 -0500

    add router to links information to section own section

--- src/pages/[slugParent]/[[...slugChild]].js
+++ src/pages/[slugParent]/[[...slugChild]].js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Link from 'next/link';
 import { Helmet } from 'react-helmet';
 
@@ -6,7 +7,7 @@ import { WebpageJsonLd } from 'lib/json-ld';
 import { helmetSettingsFromMetadata } from 'lib/site';
 import useSite from 'hooks/use-site';
 import usePageMetadata from 'hooks/use-page-metadata';
-
+import styles from '../../styles/pages/Home.module.scss';
 import Layout from 'components/Layout';
 import Header from 'components/Header';
 import Content from 'components/Content';
@@ -14,9 +15,17 @@ import Section from 'components/Section';
 import Container from 'components/Container';
 import FeaturedImage from 'components/FeaturedImage';
 import Breadcrumbs from 'components/Breadcrumbs';
+import { VscGraph } from 'react-icons/vsc';
 
-import styles from 'styles/pages/Page.module.scss';
+import { AiFillReconciliation, AiFillMail, AiOutlineFileSearch, AiOutlineGlobal } from 'react-icons/ai';
 
+import { GoArrowUp, GoArrowDown } from 'react-icons/go';
+import { MdAccountBalance, MdSmartphone } from 'react-icons/md';
+import { FaUsers, FaMapPin } from 'react-icons/fa';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import stylesPage from 'styles/pages/Page.module.scss';
+import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
 export default function Page({ page, breadcrumbs }) {
   const { title, metaTitle, description, slug, content, featuredImage, children } = page;
 
@@ -51,54 +60,400 @@ export default function Page({ page, breadcrumbs }) {
         siteTitle={siteMetadata.title}
         slug={slug}
       />
-
-      <Header>
-        {hasBreadcrumbs && <Breadcrumbs breadcrumbs={breadcrumbs} />}
-        {featuredImage && (
-          <FeaturedImage
-            {...featuredImage}
-            src={featuredImage.sourceUrl}
-            dangerouslySetInnerHTML={featuredImage.caption}
-          />
-        )}
-        <h1 className={styles.title}>{title}</h1>
-      </Header>
-
-      <Content>
-        <Section>
-          <Container>
+      {title === 'Sobre mi' && (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '80vh',
+          }}
+        >
+          <a className={styles.swipeIconUPdOW}>
             <div
-              className={styles.content}
-              dangerouslySetInnerHTML={{
-                __html: content,
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '67px',
+                position: 'absolute',
+                bottom: '-20px',
+                left: '-5px',
+                justifyContent: 'space-between',
+                fontSize: '22px',
+                color: 'white',
               }}
-            />
-          </Container>
-        </Section>
-
-        {hasChildren && (
-          <Section className={styles.sectionChildren}>
-            <Container>
-              <aside>
-                <p className={styles.childrenHeader}>
-                  <strong>{title}</strong>
+            >
+              <GoArrowUp />
+              <GoArrowDown />
+            </div>
+          </a>
+          <p
+            style={{
+              fontSize: '26px',
+              color: 'white',
+            }}
+          >
+            <RoughNotation color="orangered" type="highlight" show={true}>
+              Sobre Mi{' '}
+            </RoughNotation>
+          </p>
+          <Carousel
+            swipeable
+            swipeScrollTolerance={2}
+            transitionTime={500}
+            emulateTouch={true}
+            showArrows={false}
+            showIndicators={false}
+            axis="vertical"
+            useKeyboardArrows={true}
+            style={{ cursor: 'pointer' }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '60vh',
+                width: '100%',
+                cursor: 'n-resize;',
+              }}
+            >
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                Contadora pública colegiada, empresaria y diseñadora gráfica
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                con experiencia en temas
+                <RoughNotation color="orangered" type="circle" show={true}>
+                  {' '}
+                  tributarios{' '}
+                </RoughNotation>
+                <RoughNotation color="orange" type="circle" show={true}>
+                  ,logísticos,
+                </RoughNotation>
+                <RoughNotation color="orangered" type="circle" show={true}>
+                  {' '}
+                  financieros y{' '}
+                </RoughNotation>
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                laborales del país en rubros comerciales, publicidad, marketing,
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                transportes, asociaciones sin fines de lucro, servicios freelancer,
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                <RoughNotation color="orange" type="box" show={true}>
+                  inversiones digitales, entre otros
+                </RoughNotation>
+              </p>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '60vh',
+                width: '100%',
+                cursor: 'n-resize;',
+              }}
+            >
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                En este espacio busco asesorarte y diseñar un plan estratégico{' '}
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                para que puedas gestionar tus ,
+                <RoughNotation color="orangered" type="circle" show={true}>
+                  {' '}
+                  finanzas,
+                </RoughNotation>
+                {' mantener un '}
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>historial crediticio favorable,</p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                evitar inconvenientes con las instituciones del Estado
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>y prevenir sanciones, multas o embargos.</p>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '60vh',
+                width: '100%',
+                cursor: 'n-resize;',
+              }}
+            >
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                Entiendo que el mundo es amplio, la tecnología no para,{' '}
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>ni las inversiones ni el internet.</p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                Mientras estás leyendo esto, todos siguen operando;
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                evitar inconvenientes con las instituciones del Estado
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                así que mi intención es que puedas construir tu futuro sin temor
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                Te ofrezco brindarte un asesoramiento a medida,
+              </p>
+              <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                orientación y recomendaciones personalizadas
+              </p>
+            </div>
+          </Carousel>
+        </div>
+      )}
+      {title === 'Servicios' && (
+        <div style={{ marginRight: '0%' }}>
+          <p style={{ margin: '0', fontSize: '2rem', color: 'white' }}>
+            <RoughNotation color="orangered" type="highlight" show={true}>
+              Servicios{' '}
+            </RoughNotation>
+          </p>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              color: 'white',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '60vh',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                position: 'absolute',
+                margin: '5%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+              }}
+            >
+              <div style={{ flex: '0 32%', height: '150px', marginBottom0: '2%' }}>
+                <p
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    margin: '0',
+                    alignItems: 'center',
+                  }}
+                >
+                  <MdAccountBalance style={{ fontSize: '50px' }} color="orange" />
+                  Constitución de Empresass
                 </p>
-                <ul>
-                  {children.map((child) => {
-                    return (
-                      <li key={child.id}>
-                        <Link href={child.uri}>
-                          <a>{child.title}</a>
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </aside>
-            </Container>
-          </Section>
-        )}
-      </Content>
+              </div>
+
+              <div style={{ flex: '0 32%', height: '100px', marginBottom0: '2%' }}>
+                <p
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    margin: '0',
+                    alignItems: 'center',
+                  }}
+                >
+                  <VscGraph style={{ fontSize: '50px' }} color="orange" />
+                  Outsourcing Contable
+                </p>
+              </div>
+              <div style={{ flex: '0 32%', height: '100px', marginBottom0: '2%' }}>
+                <p
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    margin: '0',
+                    alignItems: 'center',
+                  }}
+                >
+                  <AiFillReconciliation style={{ fontSize: '50px' }} color="orange" />
+                  Planeación Tributaria
+                </p>
+              </div>
+              <div style={{ flex: '0 32%', height: '100px', marginBottom0: '2%' }}>
+                <p
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    margin: '0',
+                    alignItems: 'center',
+                  }}
+                >
+                  <AiOutlineFileSearch style={{ fontSize: '50px' }} color="orange" />
+                  Asesoramiento Tributario
+                </p>
+              </div>
+              <div style={{ flex: '0 32%', height: '100px', marginBottom0: '2%' }}>
+                <p
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    margin: '0',
+                    alignItems: 'center',
+                  }}
+                >
+                  <FaUsers style={{ fontSize: '50px' }} color="orange" />
+                  Asesoramiento Laboral
+                </p>
+              </div>
+              <div style={{ flex: '0 32%', height: '100px', marginBottom0: '2%' }}>
+                <p
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    margin: '0',
+                    alignItems: 'center',
+                  }}
+                >
+                  <AiOutlineGlobal style={{ fontSize: '50px' }} color="orange" />
+                  Asesoramiento Aduanero
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {title === 'Contacto' && (
+        <div className={styles.containerPost}>
+          <p style={{ margin: '1% 0px 0px 0px', fontSize: '2rem', color: 'white', textAlign: 'center' }}>
+            <RoughNotation color="orangered" type="highlight" show={true}>
+              Contacto{' '}
+            </RoughNotation>
+          </p>
+
+          <p>
+            P<span style={{ color: 'white' }}>{'uedo '}</span>
+            hacer mucho por tí
+          </p>
+
+          <p>
+            si quieres trabajar{' '}
+            <RoughNotation color="orange" type="circle" show={true}>
+              conmigo
+            </RoughNotation>
+          </p>
+          <p>puedes contactarme :</p>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              color: 'white',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '100%',
+              height: '30vh',
+              fontSize: '13px',
+            }}
+          >
+            <p
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                margin: '0',
+                alignItems: 'center',
+              }}
+            >
+              <FaMapPin style={{ fontSize: '20px' }} color="orange" />
+              Trujillo - Peru
+            </p>
+            <p
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                margin: '0',
+                alignItems: 'center',
+              }}
+            >
+              <MdSmartphone style={{ fontSize: '20px' }} color="orange" />
+              999 999 999
+            </p>{' '}
+            <p
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                margin: '0',
+                alignItems: 'center',
+              }}
+            >
+              <MdSmartphone style={{ fontSize: '20px' }} color="orange" />
+              044 995 454
+            </p>
+            <p
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                margin: '0',
+                alignItems: 'center',
+              }}
+            >
+              <AiFillMail style={{ fontSize: '20px' }} color="orange" />
+              l.chigne.m@gmail.coms
+            </p>
+          </div>
+        </div>
+      )}
+
+      {false && (
+        <>
+          <Header>
+            {hasBreadcrumbs && <Breadcrumbs breadcrumbs={breadcrumbs} />}
+            {featuredImage && (
+              <FeaturedImage
+                {...featuredImage}
+                src={featuredImage.sourceUrl}
+                dangerouslySetInnerHTML={featuredImage.caption}
+              />
+            )}
+            <h1 className={stylesPage.title}>{title}</h1>
+          </Header>
+
+          <Content>
+            <Section>
+              <Container>
+                <div
+                  className={stylesPage.content}
+                  dangerouslySetInnerHTML={{
+                    __html: content,
+                  }}
+                />
+              </Container>
+            </Section>
+
+            {hasChildren && (
+              <Section className={stylesPage.sectionChildren}>
+                <Container>
+                  <aside>
+                    <p className={stylesPage.childrenHeader}>
+                      <strong>{title}</strong>
+                    </p>
+                    <ul>
+                      {children.map((child) => {
+                        return (
+                          <li key={child.id}>
+                            <Link href={child.uri}>
+                              <a>{child.title}</a>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </aside>
+                </Container>
+              </Section>
+            )}
+          </Content>
+        </>
+      )}
     </Layout>
   );
 }
