6018837b4a522f1977503dd172813b537a6a7130
bdanielmr <bdanielmr@gmail.com>
2021-11-08 14:25:52 -0500

    add mobile media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,18 +33,10 @@ export default function Home({ posts, pagination, pageHome }) {
   return (
     <>
       <Layout>
-        <Section
-          style={{
-            padding: '50px 0px 0px 0px',
-
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
+        <Section style={{ position: 'relative', left: '10%', width: '80%' }}>
           <Carousel swipeable={false} emulateTouch={true} showStatus={false}>
-            <div style={{ marginRight: '1%' }} className={styles.containerPost}>
-              <p style={{ margin: '0', fontSize: '2rem', color: 'white' }}>
+            <div className={styles.containerPostOnlyMobile}>
+              <p>
                 <RoughNotation color="orangered" type="highlight" show={true}>
                   Profesional{' '}
                 </RoughNotation>
@@ -55,7 +47,7 @@ export default function Home({ posts, pagination, pageHome }) {
                 contabilidad
                 {' con asesoramiento'}
               </p>
-              <p style={{ fontSize: '2rem', fontStyle: 'italic' }}>(a medida)</p>
+              <p style={{ fontStyle: 'italic' }}>(a medida)</p>
               <p>
                 Orientacion &{' '}
                 <RoughNotation color="orange" type="circle" show={true}>
@@ -67,6 +59,310 @@ export default function Home({ posts, pagination, pageHome }) {
                   personalizadas
                 </RoughNotation>
               </p>
+            </div>
+            <div className={styles.containerPostOnlyMobile}>
+              <p style={{ margin: '0', fontSize: '2rem', color: 'white' }}>
+                <RoughNotation color="orangered" type="highlight" show={true}>
+                  Sobre Mi{' '}
+                </RoughNotation>
+              </p>
+
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  top: '50px',
+                  left: '47%',
+                  height: '60vh',
+                  position: 'absolute',
+                  justifyContent: 'space-between',
+                  fontSize: '22px',
+                  color: 'white',
+                }}
+              >
+                <GoArrowUp />
+                <GoArrowDown />
+              </div>
+
+              <Carousel
+                swipeable
+                swipeScrollTolerance={2}
+                transitionTime={500}
+                emulateTouch={true}
+                showArrows={false}
+                showIndicators={false}
+                axis="vertical"
+                useKeyboardArrows={true}
+                style={{ cursor: 'pointer' }}
+              >
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    color: 'white',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '60vh',
+                    width: '100%',
+                    cursor: 'n-resize;',
+                  }}
+                >
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>Contadora pública colegiada,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>empresaria y diseñadora gráfica</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>con experiencia en temas</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                    <RoughNotation color="orangered" type="circle" show={true}>
+                      {' '}
+                      tributarios{' '}
+                    </RoughNotation>
+                    <RoughNotation color="orange" type="circle" show={true}>
+                      ,logísticos,
+                    </RoughNotation>
+                    <RoughNotation color="orangered" type="circle" show={true}>
+                      {' '}
+                      financieros y{' '}
+                    </RoughNotation>
+                  </p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>en rubros comerciales, publicidad,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>marketing, transportes,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>asociaciones sin fines de lucro,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>servicios freelancer,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                    <RoughNotation color="orange" type="box" show={true}>
+                      inversiones digitales, entre otros
+                    </RoughNotation>
+                  </p>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    color: 'white',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '60vh',
+                    width: '100%',
+                    cursor: 'n-resize;',
+                  }}
+                >
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>En este espacio busco asesorarte </p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>diseñar un plan estratégico </p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>para que puedas</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                    gestionar tus ,
+                    <RoughNotation color="orangered" type="circle" show={true}>
+                      {' '}
+                      finanzas,
+                    </RoughNotation>
+                  </p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>mantener un historial crediticio,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>evitar inconvenientes</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>con las instituciones del Estado</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>
+                    y prevenir sanciones, multas o embargos.
+                  </p>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    color: 'white',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '60vh',
+                    width: '100%',
+                    cursor: 'n-resize;',
+                  }}
+                >
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>Entiendo que el mundo es amplio</p>
+
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>y la tecnología no para, </p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>ni las inversiones ni el internet.</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>Mientras estás leyendo esto,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>todos siguen operando;</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>evitar inconvenientes con</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>las instituciones del Estado</p>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    color: 'white',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '60vh',
+                    width: '100%',
+                    cursor: 'n-resize;',
+                  }}
+                >
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>así que mi intención es que</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>puedas construir tu futuro sin temor</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}> construir tu futuro sin temor</p>{' '}
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>Te ofrezco brindarte</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>un asesoramiento a medida,</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>orientación y recomendaciones</p>
+                  <p style={{ margin: '0', fontSize: '1rem', color: 'white' }}>personalizadas</p>
+                </div>
+              </Carousel>
+            </div>
+            <div className={styles.containerPostOnlyMobile}>
+              <p style={{ margin: '0', fontSize: '2rem', color: 'white' }}>
+                <RoughNotation color="orangered" type="highlight" show={true}>
+                  Servicios{' '}
+                </RoughNotation>
+              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  color: 'white',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: '60vh',
+                  width: '100%',
+                }}
+              >
+                <div
+                  style={{
+                    position: 'absolute',
+                    margin: '5%',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <div style={{ flex: '0 40%', height: '80px', marginBottom0: '2%' }}>
+                    <p
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        margin: '0',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <MdAccountBalance style={{ fontSize: '30px' }} color="orange" />
+                      Constitución de Empresass
+                    </p>
+                  </div>
+
+                  <div style={{ flex: '0 40%', height: '80px', marginBottom0: '2%' }}>
+                    <p
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        margin: '0',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <VscGraph style={{ fontSize: '30px' }} color="orange" />
+                      Outsourcing Contable
+                    </p>
+                  </div>
+                  <div style={{ flex: '0 40%', height: '80px', marginBottom0: '2%' }}>
+                    <p
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        margin: '0',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <AiFillReconciliation style={{ fontSize: '30px' }} color="orange" />
+                      Planeación Tributaria
+                    </p>
+                  </div>
+                  <div style={{ flex: '0 40%', height: '80px', marginBottom0: '2%' }}>
+                    <p
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        margin: '0',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <AiOutlineFileSearch style={{ fontSize: '30px' }} color="orange" />
+                      Asesoramiento Tributario
+                    </p>
+                  </div>
+                  <div style={{ flex: '0 40%', height: '80px', marginBottom0: '2%' }}>
+                    <p
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        margin: '0',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <FaUsers style={{ fontSize: '30px' }} color="orange" />
+                      Asesoramiento Laboral
+                    </p>
+                  </div>
+                  <div style={{ flex: '0 40%', height: '80px', marginBottom0: '2%' }}>
+                    <p
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        margin: '0',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <AiOutlineGlobal style={{ fontSize: '30px' }} color="orange" />
+                      Asesoramiento Aduanero
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.containerPostOnlyMobile}>
+              <p>
+                <RoughNotation color="orangered" type="highlight" show={true}>
+                  Contacto{' '}
+                </RoughNotation>
+              </p>
+
+              <p>
+                P<span style={{ color: 'white' }}>{'uedo '}</span>
+                hacer mucho por tí
+              </p>
+
+              <p>
+                si quieres trabajar{' '}
+                <RoughNotation color="orange" type="circle" show={true}>
+                  conmigo
+                </RoughNotation>
+              </p>
+              <p>puedes contactarme :</p>
+            </div>
+          </Carousel>
+        </Section>
+        <Section className={styles.sectionPostOnlyDesk}>
+          <Carousel swipeable={false} emulateTouch={true} showStatus={false}>
+            <div style={{ marginRight: '1%' }} className={styles.containerPost}>
+              <div className={styles.containerPostOnlyDesk}>
+                <p style={{ margin: '0', fontSize: '2rem', color: 'white' }}>
+                  <RoughNotation color="orangered" type="highlight" show={true}>
+                    Profesional{' '}
+                  </RoughNotation>
+                </p>
+
+                <p>
+                  E<span style={{ color: 'white' }}>{'n '}</span>
+                  contabilidad
+                  {' con asesoramiento'}
+                </p>
+                <p style={{ fontSize: '2rem', fontStyle: 'italic' }}>(a medida)</p>
+                <p>
+                  Orientacion &{' '}
+                  <RoughNotation color="orange" type="circle" show={true}>
+                    recomendaciones
+                  </RoughNotation>
+                </p>
+                <p>
+                  <RoughNotation strokeWidth={3} color="orangered" customElement="span" type="underline" show={true}>
+                    personalizadas
+                  </RoughNotation>
+                </p>
+              </div>
 
               {false && (
                 <div
